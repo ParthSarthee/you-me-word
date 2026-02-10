ad282062279c8f50652baa6684e8558c96dbbb59
Parth Sarthee <14sarthi@gmail.com>
2026-02-10 19:49:04 +0530

    feat: implement word grid and keyboard interaction in App component
    - Added state management for word grid and current position.
    - Implemented keyboard handling for letter input, backspace, and enter actions.
    - Updated CellGrid and CellRow components to display current words and highlight active cell.
    - Integrated Keyboard component to handle key presses and pass actions to the App.
    - Added tailwind-merge dependency for improved styling management.

--- you-me-word-frontend/src/App.tsx
+++ you-me-word-frontend/src/App.tsx
@@ -1,71 +1,224 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 function App() {
-	// const [count, setCount] = useState(0);
+	const [rowIndex, setRowIndex] = useState<number>(0);
+	const [colIndex, setColIndex] = useState<number>(0);
+	const [word, setWord] = useState<string[]>(["A", "P", "P", "L", "E"]);
+	const [words, setWords] = useState<string[][]>([
+		["", "", "", "", ""],
+		["", "", "", "", ""],
+		["", "", "", "", ""],
+		["", "", "", "", ""],
+		["", "", "", "", ""],
+		["", "", "", "", ""],
+	]);
+
+	useEffect(() => {
+		console.log(`Row: ${rowIndex}, Col: ${colIndex}`);
+		console.log(words.toString());
+	}, [words, rowIndex, colIndex]);
+
+	function handleKeyPress(letter: string) {
+		if (letter === "ENTER") {
+			if (rowIndex === 5) return;
+			if (colIndex < 5) return;
+			setRowIndex((prevRowIndex) => prevRowIndex + 1);
+			setColIndex(0);
+			return;
+		}
+
+		if (letter === "⌫") {
+			setWords((prevWords) => {
+				const newWords = [...prevWords];
+				newWords[rowIndex][Math.max(colIndex - 1, 0)] = "";
+				return newWords;
+			});
+
+			if (colIndex !== 0) setColIndex((prevColIndex) => prevColIndex - 1);
+
+			return;
+		}
+
+		if (colIndex === 5) return;
+
+		setWords((prevWords) => {
+			const newWords = [...prevWords];
+			newWords[rowIndex][colIndex] = letter;
+			return newWords;
+		});
+
+		if (colIndex < 5) {
+			setColIndex((prevColIndex) => prevColIndex + 1);
+		}
+	}
 
 	return (
 		<>
 			<div className="w-full h-screen flex flex-col items-center px-2 py-4 gap-4 justify-between">
-				{/* <h1>you, me and word</h1> */}
-
-				{/* <div className="flex flex-row justify-center gap-4">
-					<CellGrid cellStyle="w-4 h-4 border border-gray-300" />
-					<div className="flex flex-col gap-2">
-						<div className="flex flex-row gap-2">
-							<Cell style="w-11 h-11 bg-green-200" />
-							<Cell style="w-11 h-11 bg-orange-200" />
-						</div>
-						<div className="flex flex-row gap-2">
-							<Cell style="w-11 h-11 bg-gray-200" />
-							<Cell style="w-11 h-11 border-2 border-gray-200" />
-						</div>
-					</div>
-				</div> */}
-
 				{/* Opponent's grid */}
-				<CellGrid cellStyle="w-6 h-4 border border-gray-300" />
+				<CellGrid
+					cellStyle="w-6 h-4 border border-gray-300"
+					words={words}
+					rowIndex={rowIndex}
+					colIndex={colIndex}
+				/>
 
 				{/* My own grid */}
 				<div className="flex-1 flex items-center">
-					<CellGrid />
+					<CellGrid
+						cellStyle="w-12 h-12 border-2 border-gray-300"
+						words={words}
+						rowIndex={rowIndex}
+						colIndex={colIndex}
+					/>
 				</div>
 
-				<Keyboard />
+				<Keyboard handleKeyPress={handleKeyPress} />
 			</div>
 		</>
 	);
 }
 
-function CellGrid({ cellStyle = "w-12 h-12 border-2 border-gray-300" }) {
+function CellGrid({
+	cellStyle,
+	words,
+	rowIndex,
+	colIndex,
+}: {
+	cellStyle: string;
+	words: string[][];
+	rowIndex: number;
+	colIndex: number;
+}) {
 	return (
 		<div className="flex justify-center items-center">
 			<div className="flex flex-col gap-1 max-w-sm">
-				<CellRow cellStyle={cellStyle} />
-				<CellRow cellStyle={cellStyle} />
-				<CellRow cellStyle={cellStyle} />
-				<CellRow cellStyle={cellStyle} />
-				<CellRow cellStyle={cellStyle} />
-				<CellRow cellStyle={cellStyle} />
+				<CellRow
+					cellStyle={cellStyle}
+					row={0}
+					words={words}
+					rowIndex={rowIndex}
+					colIndex={colIndex}
+				/>
+				<CellRow
+					cellStyle={cellStyle}
+					row={1}
+					words={words}
+					rowIndex={rowIndex}
+					colIndex={colIndex}
+				/>
+				<CellRow
+					cellStyle={cellStyle}
+					row={2}
+					words={words}
+					rowIndex={rowIndex}
+					colIndex={colIndex}
+				/>
+				<CellRow
+					cellStyle={cellStyle}
+					row={3}
+					words={words}
+					rowIndex={rowIndex}
+					colIndex={colIndex}
+				/>
+				<CellRow
+					cellStyle={cellStyle}
+					row={4}
+					words={words}
+					rowIndex={rowIndex}
+					colIndex={colIndex}
+				/>
+				<CellRow
+					cellStyle={cellStyle}
+					row={5}
+					words={words}
+					rowIndex={rowIndex}
+					colIndex={colIndex}
+				/>
 			</div>
 		</div>
 	);
 }
 
-function CellRow({ cellStyle = "w-12 h-12 border-2 border-gray-300" }) {
+function CellRow({
+	cellStyle,
+	row,
+	words,
+	rowIndex,
+	colIndex,
+}: {
+	cellStyle: string;
+	row: number;
+	words: string[][];
+	rowIndex: number;
+	colIndex: number;
+}) {
 	return (
 		<div className="flex flex-row items-center gap-1">
-			<Cell style={cellStyle} />
-			<Cell style={cellStyle} />
-			<Cell style={cellStyle} />
-			<Cell style={cellStyle} />
-			<Cell style={cellStyle} />
+			<Cell
+				style={cellStyle}
+				row={row}
+				col={0}
+				value={words[row][0]}
+				rowIndex={rowIndex}
+				colIndex={colIndex}
+			/>
+			<Cell
+				style={cellStyle}
+				row={row}
+				col={1}
+				value={words[row][1]}
+				rowIndex={rowIndex}
+				colIndex={colIndex}
+			/>
+			<Cell
+				style={cellStyle}
+				row={row}
+				col={2}
+				value={words[row][2]}
+				rowIndex={rowIndex}
+				colIndex={colIndex}
+			/>
+			<Cell
+				style={cellStyle}
+				row={row}
+				col={3}
+				value={words[row][3]}
+				rowIndex={rowIndex}
+				colIndex={colIndex}
+			/>
+			<Cell
+				style={cellStyle}
+				row={row}
+				col={4}
+				value={words[row][4]}
+				rowIndex={rowIndex}
+				colIndex={colIndex}
+			/>
 		</div>
 	);
 }
 
-function Cell({ style = "w-12 h-12 border-2 border-gray-300", value = "" }) {
+function Cell({
+	style,
+	value,
+	row,
+	col,
+	rowIndex,
+	colIndex,
+}: {
+	style?: string;
+	value?: string;
+	row: number;
+	col: number;
+	rowIndex: number;
+	colIndex: number;
+}) {
+	// if (rowIndex === row && colIndex === col) {
+	// }
+
 	return (
 		<div
 			className={`${style} flex justify-center items-center p-1 text-3xl font-bold`}
@@ -75,7 +228,11 @@ function Cell({ style = "w-12 h-12 border-2 border-gray-300", value = "" }) {
 	);
 }
 
-function Keyboard() {
+function Keyboard({
+	handleKeyPress,
+}: {
+	handleKeyPress: (letter: string) => void;
+}) {
 	const row1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
 	const row2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
 	const row3 = ["Z", "X", "C", "V", "B", "N", "M"];
@@ -85,32 +242,42 @@ function Keyboard() {
 			{/* First row */}
 			<div className="flex flex-row gap-1 justify-center">
 				{row1.map((letter) => (
-					<Key key={letter} letter={letter} />
+					<Key handleKeyPress={handleKeyPress} key={letter} letter={letter} />
 				))}
 			</div>
 
 			{/* Second row */}
 			<div className="flex flex-row gap-1 justify-center">
 				{row2.map((letter) => (
-					<Key key={letter} letter={letter} />
+					<Key handleKeyPress={handleKeyPress} key={letter} letter={letter} />
 				))}
 			</div>
 
 			{/* Third row */}
 			<div className="flex flex-row gap-1 justify-center">
-				<Key letter="ENTER" isWide />
+				<Key handleKeyPress={handleKeyPress} letter="ENTER" isWide />
 				{row3.map((letter) => (
-					<Key key={letter} letter={letter} />
+					<Key handleKeyPress={handleKeyPress} key={letter} letter={letter} />
 				))}
-				<Key letter="⌫" isWide />
+				<Key handleKeyPress={handleKeyPress} letter="⌫" isWide />
 			</div>
 		</div>
 	);
 }
 
-function Key({ letter = "", isWide = false }) {
+function Key({
+	letter = "",
+	isWide = false,
+	handleKeyPress,
+}: {
+	letter: string;
+	isWide?: boolean;
+	handleKeyPress?: (letter: string) => void;
+}) {
 	function handleClick() {
-		console.log(`Key pressed: ${letter}`);
+		if (handleKeyPress) {
+			handleKeyPress(letter);
+		}
 	}
 
 	return (
